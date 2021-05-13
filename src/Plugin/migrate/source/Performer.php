3bde9d9d4291d91860856a91a6207eeda257018a
Jason Loeffler <j@minorscience.com>
2021-05-13 13:48:58 -0400

    ~ performer

--- src/Plugin/migrate/source/Performer.php
+++ src/Plugin/migrate/source/Performer.php
@@ -14,23 +14,23 @@ use Drupal\migrate\Plugin\migrate\source\SqlBase;
  */
 class Performer extends SqlBase {
 
-
   /**
    * {@inheritdoc}
    */
   public function query() {
-    $query = $this->select('track', 't')
-      ->fields('t', [
+    $query = $this->select('artist_item', 'ai')
+      ->fields('ai', [
         'id',
-        'title',
-      ]);
-      $query->join('artist_item','ai', 't.id = ai.item_id');
-      $query->fields('ai', [
         'artist_id',
-        'item_id'
-      ])->range(0,3);
+        'instrument_id',
+        'item_id',
+        'item_table'
+      ])
+      ->condition('item_table', 'track', '=')
+      ->orderBy('item_id','ASC')
+      ->range(0,10);
 
-    return $query;
+      return $query;
   }
 
   /**
@@ -38,9 +38,11 @@ class Performer extends SqlBase {
    */
   public function fields() {
     $fields = [
-      'id' => $this->t('DRAM identifier'),
-      'title' => $this->t('Title'),
+      'id' => $this->t('Unique identifier'),
+      'item_id' => $this->t('Item identifier'),
       'artist_id' => $this->t('Artist identifier'),
+      'item_table' => $this->t('Item table'),
+      'instrument' => $this->t('Instrument identifier'),
     ];
     return $fields;
   }
@@ -51,46 +53,9 @@ class Performer extends SqlBase {
    */
   public function prepareRow(Row $row) {
     parent::prepareRow($row);
-    // 1. var_dump shows the content of the $row object.
-    // var_dump($row->getSourceIdValues($source));
 
-    // 2. renders an empty array
-
-    // 3. renders the source array
     $source = $row->getSourceProperty('source');
-    // echo(gettype($source) . PHP_EOL);
-
-    // 4. retrieves value from the artist_id source property
-    // var_dump($row->getSourceProperty('artist_id'));
-    $source_artist_id = $row->getSourceProperty('artist_id');
-
-    // echo ('the artist_id type is: ' . gettype($source_artist_id) . PHP_EOL);
-    // echo ('the artist_id is: ' . $source_artist_id . PHP_EOL);
-    // echo (gettype($artist_ids . PHP_EOL));
-
-    // foreach ($artist_ids as $source_artist_id) {
-    //   echo ('this is the source artist id: ' . $source_artist_id . PHP_EOL);
-    // }
-
-    // $artist_ids = $this->select('artist_item', 'ai')
-    //   ->fields('ai', ['artist_id'])
-    //   ->condition('id', $row->getSourceProperty('item_id'), '=')
-    //   ->execute()
-    //   ->fetchCol();
-    // $row->setSourceProperty('artists', $artist_ids);
-
-    // $foo = $row->getSourceProperty('item_id');
-    // var_dump($foo);
-    // echo ($foo . PHP_EOL);
-
-    $artist_ids = $this->select('artist_item', 'ai')
-      ->fields('ai', ['artist_id'])
-      ->condition('item_id', $row->getSourceProperty('item_id'), '=')
-      ->execute()
-      ->fetchCol();
-    $row->setSourceProperty('names', $artist_ids);
-    var_dump($artist_ids);
-    var_dump($item_id);
+    // var_dump($row);
   }
 
   /**
@@ -100,8 +65,9 @@ class Performer extends SqlBase {
     return [
       'id' => [
         'type' => 'integer',
-        'alias' => 'ai',
+        'alias' => 'id'
       ],
     ];
-  }
 }
+}
+
