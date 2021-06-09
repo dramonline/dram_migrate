1f2ccf90c3c515d2e289b51955f5299e92dcfba4
Jason Loeffler <j@minorscience.com>
2021-06-09 18:42:17 -0400

    this is a working example of ensemble entity references

--- src/Plugin/migrate/source/Track.php
+++ src/Plugin/migrate/source/Track.php
@@ -19,19 +19,8 @@ class Track extends SqlBase {
    * {@inheritdoc}
    */
   public function query() {
-    $query = $this->select('track_test', 't')
-      ->fields('t', [
-        'id',
-        'legacy_id',
-        'title',
-      ]);
-      $query->join('artist_item','ai', 't.id = ai.item_id');
-      $query->fields('ai', [
-        'artist_id',
-        'item_id'
-      ]);
-
-    return $query;
+    return $this->select('track_test', 't')
+      ->fields('t', ['id','legacy_id','title']);
   }
 
   /**
@@ -51,14 +40,14 @@ class Track extends SqlBase {
    * {@inheritdoc}
    */
   public function prepareRow(Row $row) {
-    parent::prepareRow($row);
+     $track_ids = $row->getSourceProperty('id');
 
-    $performer_ids = $this->select('artist_item', 'ai')
-      ->fields('ai', ['id'])
-      ->condition('item_id', $row->getSourceProperty('item_id'), '=')
+    $ensembles = $this->select('ensemble_item_test', 'eit')
+      ->fields('eit', ['artist_id'])
+      ->condition('eit.item_id', $track_ids)
       ->execute()
       ->fetchCol();
-    $row->setSourceProperty('performer_ids', $performer_ids);
+    $row->setSourceProperty('ensemble_ids', $ensembles);
 
   }
 
