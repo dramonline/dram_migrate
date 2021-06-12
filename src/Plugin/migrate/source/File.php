33af15e7dc01828c06618ecc20b6778a530ed34d
Jason Loeffler <j@minorscience.com>
2021-06-11 23:48:10 -0400

    initial commit

---
+++ src/Plugin/migrate/source/File.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\dram_migrate\Plugin\migrate\source;
+
+use Drupal\migrate\Plugin\migrate\source\SqlBase;
+
+/**
+ * Minimalistic example for a SqlBase source plugin.
+ *
+ * @MigrateSource(
+ *   id = "ensemble"
+ * )
+ */
+class Ensemble extends SqlBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    $query = $this->select('ensemble', 'e')
+      ->fields('e', [
+        'id',
+        'legacy_id',
+        'url_code',
+        'name',
+        'active',
+        'type'
+      ])->orderBy('name');
+
+    return $query;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [
+      'id' => $this->t('DRAM identifier')
+    ];
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIds() {
+    return [
+      'id' => [
+        'type' => 'integer',
+        'alias' => 'e',
+      ],
+    ];
+  }
+}
